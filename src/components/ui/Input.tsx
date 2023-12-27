5951186987e235ea320a0a3725c07fa39b2415fe
ianabreu <ianabreudev@gmail.com>
2023-12-27 05:02:32 -0300

    feat: add ref to Input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, TextareaHTMLAttributes } from "react";
+import { InputHTMLAttributes, TextareaHTMLAttributes, forwardRef } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
@@ -7,9 +7,14 @@ interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
 const defaultStyles =
   "mb-4 rounded-lg bg-input text-foreground p-4 border-[1px] placeholder:text-opacity-80";
 
-export function Input({ ...rest }: InputProps) {
-  return <input className={`${defaultStyles} h-10`} {...rest} />;
-}
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ ...rest }, ref) => {
+    return <input className={`${defaultStyles} h-10`} {...rest} ref={ref} />;
+  }
+);
+
 export function TextArea({ ...rest }: TextAreaProps) {
   return <textarea className={`${defaultStyles}`} {...rest} />;
 }
+
+Input.displayName = "Input";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useRef } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import logoImg from "@/../public/logo.svg";
@@ -5,8 +6,24 @@ import Image from "next/image";
 
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
+import { useAuth } from "@/contexts/AuthContext";
 
 export default function Home() {
+  const { signIn } = useAuth();
+  const inputEmail = useRef<HTMLInputElement | null>(null);
+  const inputPassword = useRef<HTMLInputElement | null>(null);
+
+  async function handleSignIn(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const email = inputEmail.current?.value;
+    const password = inputPassword.current?.value;
+
+    if (!email || !password) return;
+
+    await signIn({ email, password });
+    inputEmail.current !== null ? (inputEmail.current.value = "") : "";
+    inputPassword.current !== null ? (inputPassword.current.value = "") : "";
+  }
   return (
     <>
       <Head>
@@ -17,9 +34,17 @@ export default function Home() {
       >
         <Image src={logoImg} alt="Logo" />
         <div className="w-full max-w-screen-sm mt-8 flex flex-col items-center my-8">
-          <form className="flex flex-col w-[90%]">
-            <Input type="email" placeholder="Digite seu email" />
-            <Input type="password" placeholder="Digite sua senha" />
+          <form className="flex flex-col w-[90%]" onSubmit={handleSignIn}>
+            <Input
+              type="email"
+              placeholder="Digite seu email"
+              ref={inputEmail}
+            />
+            <Input
+              type="password"
+              placeholder="Digite sua senha"
+              ref={inputPassword}
+            />
             <Button loading={false} size="large">
               Acessar
             </Button>
