d9aff2ccede20809e0690fc118759994fd49cadc
ianabreu <ianabreudev@gmail.com>
2024-01-05 19:48:32 -0300

    fix: ajust imports components

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,8 +1,8 @@
-import { Dialog } from "./alert-modal";
-import { Input, TextArea } from "./input";
-import { Button } from "./button";
-import { NavLink } from "./header-link";
-import { Logo } from "./logo";
-import { Title } from "./title";
+import { Dialog } from "@/components/ui/alert-modal";
+import { Input, TextArea } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { NavLink } from "@/components/ui/header-link";
+import { Logo } from "@/components/ui/logo";
+import { Title } from "@/components/ui/title";
 
 export { Dialog, Button, Input, TextArea, NavLink, Logo, Title };
