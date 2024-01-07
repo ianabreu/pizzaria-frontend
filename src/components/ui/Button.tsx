d7ef622b4b8dfa13f6a6d07f7f315f72a4c08776
ianabreu <ianabreudev@gmail.com>
2024-01-07 05:05:48 -0300

    deploy test

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,12 +1,12 @@
-import { ButtonHTMLAttributes, ReactNode } from "react";
+// import { ButtonHTMLAttributes, ReactNode } from "react";
 import { Icons } from "../../constants/Icons";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  loading?: boolean;
-  children?: ReactNode;
-  size?: "default" | "large";
-  variant?: keyof typeof colorStyles;
-}
+// interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+//   loading?: boolean;
+//   children?: ReactNode;
+//   size?: "default" | "large";
+//   variant?: keyof typeof colorStyles;
+// }
 
 const defaultStyles = `w-full p-[0.4rem] rounded-lg transition-all duration-200 font-bold`;
 const hoverStyles = `hover:filter hover:brightness-110 `;
@@ -22,13 +22,15 @@ export function UIButton({
   children,
   variant = "primary",
   ...rest
-}: ButtonProps) {
+}: any) {
   return (
     <button
       disabled={loading}
-      className={`${defaultStyles} ${colorStyles[variant]} ${hoverStyles} ${
-        loading && "cursor-not-allowed"
-      } ${size === "large" ? "h-10 text-lg" : ""} `}
+      className={`${defaultStyles} ${
+        colorStyles[variant as keyof typeof colorStyles]
+      } ${hoverStyles} ${loading && "cursor-not-allowed"} ${
+        size === "large" ? "h-10 text-lg" : ""
+      } `}
       {...rest}
     >
       <a>{loading ? <span>{Icons["loading"]}</span> : children}</a>
